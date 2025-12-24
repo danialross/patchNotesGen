1bab098265c84e3f9f8daaba34529d7238b5b657
danial-r0ss <muhammaddanialross@ifca.com.my>
2025-12-24 13:45:32 +0800

    fix: add min h to commit textfield

--- src/components/input/CommitTextField.tsx
+++ src/components/input/CommitTextField.tsx
@@ -1,19 +1,22 @@
-import {Label} from "@radix-ui/react-label";
-import {Textarea} from "@/components/ui/textarea.tsx";
-import type {Dispatch, SetStateAction} from "react";
+import { Label } from "@radix-ui/react-label";
+import { Textarea } from "@/components/ui/textarea.tsx";
+import type { Dispatch, SetStateAction } from "react";
 
 type Props = {
-	setValue : Dispatch<SetStateAction<string>>;
-}
+  setValue: Dispatch<SetStateAction<string>>;
+};
 
-export const CommitTextField = ({setValue}:Props) => {
-return <>
-	<Label className={"text-sm px-2 font-medium"}>Commits</Label>
-	<Textarea
-		className={
-			"border-2 border-[#FFC670] bg-white resize-none w-full h-[330px] focus-visible:ring-[#FFC670] focus-visible:border-[#FF9D0A]"
-		}
-		placeholder="Type commit messages here."
-		onChange={(e) => setValue(e.target.value)}
-	/></>
-}
+export const CommitTextField = ({ setValue }: Props) => {
+  return (
+    <>
+      <Label className={"text-sm px-2 font-medium"}>Commits</Label>
+      <Textarea
+        className={
+          "border-2 border-[#FFC670] bg-white resize-none w-full h-full min-h-[320px] focus-visible:ring-[#FFC670] focus-visible:border-[#FF9D0A]"
+        }
+        placeholder="Type commit messages here."
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </>
+  );
+};
